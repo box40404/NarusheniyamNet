ec547dcc5cbc1cd512320eca982cd8e7be981904
box40404 <pashayashik@mail.ru>
2024-09-08 19:21:45 +0300

    add design

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,43 +1,56 @@
 <x-layout>
     <x-slot:title> Зарегистрироваться </x-slot:title>
+    <x-slot:styles>@vite('resources/css/auth/register.css')</x-slot:styles>
 
-    <form action="{{ route('register') }}" method="post">
-        @csrf
-        <label>
-            <p>ФИО</p>
-            <input type="text" name="full_name" value="{{ old('full_name') }}" required>
-        </label>
+    <section class="register-wrap">
+        <div class="register-container">
+            <h2>Зарегистрируйтесь</h2>
+            <form action="{{ route('register') }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="full_name">ФИО</label>
+                    @error('full_name')
+                        <div class="error-message"> {{ $message }} </div>
+                    @enderror
+                    <input type="text" name="full_name" value="{{ old('full_name') }}" required>
+                    <div class="error-message">{{session('error')}}</div>
+                </div>
 
-        <label>
-            <p>Логин</p>
-            <input type="text" name="username" value="{{ old('username') }}" required>
-        </label>
+                <div class="form-group">
+                    <label for="username">Логин</label>
+                    @error('username')
+                        <div class="error-message"> {{ $message }} </div>
+                    @enderror
+                    <input type="text" name="username" value="{{ old('username') }}" required>
+                </div>
 
-        <label>
-            <p>Email</p>
-            <input type="email" name="email" value="{{ old('email') }}" required>
-        </label>
+                <div class="form-group">
+                    <label for="email">Почта</label>
+                    @error('email')
+                        <div class="error-message"> {{ $message }} </div>
+                    @enderror
+                    <input type="email" name="email" value="{{ old('email') }}" required>
+                </div>
 
-        <label>
-            <p>Телефон</p>
-            <input type="text" name="phone" value="{{ old('phone') }}" required>
-        </label>
+                <div class="form-group">
+                    <label for="phone">Телефон</label>
+                    @error('phone')
+                        <div class="error-message"> {{ $message }} </div>
+                    @enderror
+                    <input type="text" name="phone" value="{{ old('phone') }}" required>
+                </div>
 
-        <label>
-            <p>Пароль</p>
-            <input type="password" name="password" required>
-        </label>
-
-        <p><input type="submit" value="Зарегистрироваться"></p>
-    </form>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+                <div class="form-group">
+                    <label for="password">Пароль</label>
+                    @error('password')
+                        <div class="error-message"> {{ $message }} </div>
+                    @enderror
+                    <input type="password" name="password" required>
+                </div>
+               
+                <button type="submit" class="btn">Зарегистрироваться</button> 
+            </form>
         </div>
-    @endif
+    </section>
+
 </x-layout>
